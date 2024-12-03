6fcf45409b31f77ac7c62778b98aeab867b8371c
sbiggs <scott.biggs@consensus-builder.com>
2024-12-03 15:43:54 -0600

    added ability to copy text from the output. cleaned up a little.

--- app/src/main/java/com/sleepfuriously/mypostman/MainActivity.kt
+++ app/src/main/java/com/sleepfuriously/mypostman/MainActivity.kt
@@ -18,8 +18,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,7 +35,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -73,14 +72,11 @@ class MainActivity : ComponentActivity() {
                 var headerKey by remember { mutableStateOf("") }
                 var headerValue by remember { mutableStateOf("") }
 
-                // sliders
+                // for slider
                 var trustAll by remember { mutableStateOf(false) }
 
-//                /** When true, show all text with big fonts */
-//                val bigFont = remember { mutableStateOf(true) }
-//                val bigFontSize = remember { mutableIntStateOf(BIG_FONT_SIZE) }
-
                 val landscape = LocalConfiguration.current.orientation == ORIENTATION_LANDSCAPE
+
 
                 Scaffold(
                     modifier = Modifier
@@ -95,7 +91,7 @@ class MainActivity : ComponentActivity() {
                         LazyColumn(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .padding(14.dp)
+                                .padding(8.dp)
                                 .safeContentPadding()      // takes the insets into account (nav bars, etc)
                         ) {
                             item {
@@ -256,7 +252,9 @@ class MainActivity : ComponentActivity() {
                                         R.string.success_equals),
                                         fontWeight = FontWeight.Bold
                                     )
-                                    Text(responseSuccess.toString())
+                                    SelectionContainer {
+                                        Text(responseSuccess.toString())
+                                    }
                                 }
                             }
                             item {
@@ -265,7 +263,9 @@ class MainActivity : ComponentActivity() {
                                         stringResource(R.string.code_equals),
                                         fontWeight = FontWeight.Bold
                                     )
-                                    Text(responseCode.toString())
+                                    SelectionContainer {
+                                        Text(responseCode.toString())
+                                    }
                                 }
                             }
                             item {
@@ -274,7 +274,9 @@ class MainActivity : ComponentActivity() {
                                         stringResource(R.string.message_equals),
                                         fontWeight = FontWeight.Bold
                                     )
-                                    Text(responseMessage)
+                                    SelectionContainer {
+                                        Text(responseMessage)
+                                    }
                                 }
                             }
                             item {
@@ -283,24 +285,13 @@ class MainActivity : ComponentActivity() {
                                         stringResource(R.string.body_equals),
                                         fontWeight = FontWeight.Bold
                                     )
-                                    Text(responseBody)
+                                    SelectionContainer {
+                                        Text(responseBody)
+                                    }
                                 }
                             }
                         }
                     }
-
-//                    DrawBigFontButton(
-//                        bigFont = bigFont.value,
-//                        onClickAction = {
-//                            bigFont.value = !bigFont.value
-//                            if (bigFont.value) {
-//                                bigFontSize.intValue = BIG_FONT_SIZE
-//                            }
-//                            else {
-//                                bigFontSize.intValue = REGULAR_FONT_SIZE
-//                            }
-//                        }
-//                    )
                 }
             }
         }
@@ -336,18 +327,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-/*
-@Composable
-fun BigText(text : String, fontSize: Int = BIG_FONT_SIZE, bold: Boolean = false) {
-    if (bold) {
-        Text(text, fontSize = fontSize.sp, fontWeight = FontWeight.Bold)
-    }
-    else {
-        Text(text, fontSize = fontSize.sp)
-    }
-}
-*/
 
 @Composable
 private fun SwitchWithLabel(modifier: Modifier = Modifier, label: String, state: Boolean, onStateChange: (Boolean) -> Unit) {
